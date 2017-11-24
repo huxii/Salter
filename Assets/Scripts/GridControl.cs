0e9d8e7da5c2834824dd458e6db110b30de8bbfe
Xin Hu <xh1041@nyu.edu>
2017-11-24 15:29:34 -0500

    Initialized

---
+++ Assets/Scripts/GridControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridControl : MonoBehaviour 
+{
+	public GameObject[] models;
+	public int gridWidth = 10;
+	public int gridLength = 10;
+	public int gridHeight = 1;
+	public float unitSize = 1.0f;
+	public bool[, , ] colliderMap;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InitGrid();
+		UpdateGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void InitGrid()
+	{
+		colliderMap = new bool[gridWidth, gridLength, gridHeight];
+		for (int x = 0; x < gridWidth; ++x)
+		{
+			for (int y = 0; y < gridHeight; ++y)
+			{
+				for (int z = 0; z < gridLength; ++z)
+				{
+					colliderMap[x, z, y] = true;
+				}
+			}
+		}
+	}
+
+	void UpdateGrid()
+	{
+		if (models.Length <= 0)
+		{
+			return;
+		}
+
+		int startX = -gridWidth / 2;
+		int endX = gridWidth - gridWidth / 2;
+		int startY = 0;
+		int endY = gridHeight;
+		int startZ = -gridLength / 2;
+		int endZ = gridLength - gridLength / 2;
+
+		for (int x = startX; x < endX; ++x)
+		{
+			float X = x * unitSize + unitSize / 2;
+			for (int y = startY; y < endY; ++y)
+			{
+				float Y = y * unitSize + unitSize / 2;
+				for (int z = startZ; z < endZ; ++z)
+				{
+					float Z = z * unitSize + unitSize / 2;
+					for (int i = 0; i < models.Length; ++i)
+					{
+						if (models[i].GetComponent<BoxCollider>().bounds.Contains(new Vector3(X, Y, Z)) == false)
+						{
+							colliderMap[x - startX, z - startZ, y - startY] = false;
+						}
+						else
+						{
+							Debug.Log("x: " + x + " y: " + y + " z: " + z + " X:" + X + " Y:" + Y + " Z:" + Z);
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void OnMouseDown()
+	{
+	}
+
+	void OnMouseDrag()
+	{
+		RaycastHit hitInfo;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hitInfo))
+		{
+			if (hitInfo.collider.gameObject.tag == "EmptyBlock")
+			{
+				Debug.Log(true);
+				transform.position = hitInfo.collider.gameObject.transform.position;
+			}
+			else
+			{
+				Debug.Log(false);
+			}
+		}		
+	}
+
+	/*
+	void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+ 
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+ 
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+ 
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+ 
+	}
+	*/
+
+	/*
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 5.0f));
+		transform.position = targetPos;
+	}
+	*/
+}
