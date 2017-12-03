acce6e4a5ba95831f4819b1c48df7d8b672e88be
Xin Hu <xh1041@nyu.edu>
2017-12-03 18:02:36 -0500

    II: scene transitions completed

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class LevelControl : SceneLoader
+{
+	public SpriteRenderer blank;
+	public Transform mainCam;
+	public Transform tweenCam;
+
+	GameObject [] audios;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartLevel(blank);
+		TweenCamera(mainCam, tweenCam);
+
+		audios = GameObject.FindGameObjectsWithTag("Sound");
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void RestartLevel()
+	{
+		LoadLevel(blank, SceneManager.GetActiveScene().buildIndex, 0, 1f);
+		for (int i = 0; i < audios.Length; ++i)
+		{
+			audios[i].GetComponent<AudioSource>().DOFade(0f, 1f);
+		}
+	}
+
+	public void BackToTitle()
+	{
+		LoadLevel(blank, 0, 0, 1f);
+		for (int i = 0; i < audios.Length; ++i)
+		{
+			audios[i].GetComponent<AudioSource>().DOFade(0f, 1f);
+		}
+	}
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,26 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class SceneLoader : MonoBehaviour
 {
-	public void LoadScene(int level)
+	void FadeIn(SpriteRenderer blank, float duration = 1.0f)
+	{
+		blank.DOFade(0f, duration);
+	}
+
+	void FadeOut(SpriteRenderer blank, float duration = 1.0f)
+	{
+		blank.DOFade(1f, duration);
+	}
+
+	public void LoadLevel(SpriteRenderer blank, int level, float delayDuration = 2.0f, float fadeDuration = 1.0f)
 	{ 
-		Application.LoadLevel(level);
+		StartCoroutine(DelayToLoadLevel(level, delayDuration + fadeDuration));
+		StartCoroutine(DelayToFade(blank, delayDuration, fadeDuration));
 	}
 
-	public void RestartGame()
+	public void StartLevel(SpriteRenderer blank)
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+		FadeIn(blank, 3.0f);
 	}
 
-	public void BackToTitle()
+	public void TweenCamera(Transform mainCam, Transform tweenCam, float totalDuration = 3.0f)
 	{
-		SceneManager.LoadScene(0);
+		mainCam.DOMove(tweenCam.position, totalDuration).SetEase(Ease.InOutCubic);
+		mainCam.DORotate(tweenCam.rotation.eulerAngles, totalDuration).SetEase(Ease.InOutCubic);		
 	}
-
+		
 	public void ExitGame()
 	{
 		Application.Quit();
+	}
+
+	public IEnumerator DelayToLoadLevel(int level, float delaySeconds)
+	{
+		yield return new WaitForSeconds(delaySeconds);
+		Application.LoadLevel(level);
+	}
+
+	public IEnumerator DelayToFade(SpriteRenderer blank, float delaySeconds, float fadeDuration)
+	{
+		yield return new WaitForSeconds(delaySeconds);
+		FadeOut(blank, fadeDuration);
 	}
 }
