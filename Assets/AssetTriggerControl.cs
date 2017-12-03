4b0faa31203dd4fdc0577b98b23b3478a8512cee
Xin Hu <xh1041@nyu.edu>
2017-12-02 21:20:48 -0500

    II: Bower added

--- Assets/AssetTriggerControl.cs
+++ Assets/AssetTriggerControl.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlowerControl : MonoBehaviour
+public class AssetTriggerControl : MonoBehaviour
 {
+	public bool randomSpeed = false;
+
 	Animator animator;
 	bool watered;
 	float speed;
@@ -14,24 +16,31 @@ public class FlowerControl : MonoBehaviour
 		animator = GetComponent<Animator>();
 		watered = false;
 		animator.SetBool("Watered", watered);
-		speed = Random.Range(0.5f, 0.7f);
-		animator.speed = speed;
+
+		if (randomSpeed)
+		{
+			speed = Random.Range(0.5f, 0.7f);
+			animator.speed = speed;
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update()
 	{
-		
+
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Water"))
 		{
-			if (!watered)
+			if (other.gameObject.GetComponent<WaterBehavior>() && other.gameObject.GetComponent<WaterBehavior>().isAwake)
 			{
-				watered = true;
-				animator.SetBool("Watered", watered);
+				if (!watered)
+				{
+					watered = true;
+					animator.SetBool("Watered", watered);
+				}
 			}
 		}
 	}
