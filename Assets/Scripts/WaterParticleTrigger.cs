21270952dc552bc83fa5c6081df9a84407616119
Shirley Huang <shirley.huang@nyu.edu>
2017-12-04 20:48:01 -0500

    added water particle behavior

---
+++ Assets/Scripts/WaterParticleTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterParticleTrigger : MonoBehaviour{
+	public GameObject waterPariticlePF;
+	private bool isParticleTriggered;
+
+	void Start(){
+		isParticleTriggered = false;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("Water")){
+			if (gameObject.CompareTag("EmptyBlock") && other.gameObject.GetComponent<WaterBehavior>().isAwake){
+				if (!isParticleTriggered) {
+					isParticleTriggered = true;
+					GameObject particle = (GameObject)Instantiate(waterPariticlePF);
+					particle.transform.parent = gameObject.transform;
+					particle.transform.localPosition = new Vector3 (0, 0.3f, 0);
+				}
+			}
+		}
+	}
+
+}
+
